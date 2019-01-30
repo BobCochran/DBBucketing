ec86ba8d5286edeb9653675486e37262f9c00a42
BobCochran <r2cochran2@gmail.com>
2019-01-29 20:19:57 -0500

    Shorten database and collection names. Reduce test sample count. Comment the code with my understanding of how it performs.

--- TestMongo.cs
+++ TestMongo.cs
@@ -1,7 +1,21 @@
 using System;
 using MongoDB.Bson;
 using MongoDB.Driver;
-
+/* Original code created by Ric Itto. 
+ * 
+ * This code is a C# implementation of "sized-based bucketing" as discussed
+ * in the blog post "Time-Series Data and MongoDB: Part 2 - Schema Design"
+ * (https://www.mongodb.com/blog/post/time-series-data-and-mongodb-part-2-schema-design-best-practices) 
+ * by Robert Walters. September 13, 2018.
+ *
+ * This code changed by Bob Cochran (r2cochran2@gmail.com, BobCochran on GitHub) 
+ * to do the following:
+ *
+ * 1. Shorten database and collection names.
+ * 2. Reduce the number of samples bucketed within the document, 
+ *    so that output collection documents can be studied more easily.
+ *
+ */
 namespace TestMongo
 {
 
@@ -15,12 +29,24 @@ namespace TestMongo
    {
        static void Main()
        {
+
+           // Set up database connection. 
            const string connectionString = "mongodb://localhost:27017";
            var client = new MongoClient(connectionString);
-           var db = client.GetDatabase("SizeBasedtimeSeriesBase");
-           var collection = db.GetCollection<BsonDocument>("SizeBasedtimeSeries");
+           var db = client.GetDatabase("itto");
 
-            for (var i = 0; i < 100; i++)
+           // Work with this collection.
+           var collection = db.GetCollection<BsonDocument>("szbase1");
+
+           /* The below code will generate 0 to n test samples for adding
+            * to the document that is generated. So suppose that the value
+            * of the field 'nsamples' is set to 3, and the for loop below loops
+            * until i < 4. This will cause two documents to be written to the
+            * collection: Test samples 0, 1, 2 are written to the first document.
+            * Because the insert is coded with upsert=true, a second document 
+            * is then created and it contains test sample 3.
+            */ 
+            for (var i = 0; i < 4; i++)
            {
                UpdateWithNewTest(collection, "toto", new TestPayLoad() { Test = i });
                // Thread.Sleep(100); // to go slower
@@ -28,11 +54,16 @@ namespace TestMongo
        }
 
 
+        /* If no collection exists and upsert=true, this will cause a new collection
+         * and a new document to be created. This code actually seems to assume this
+         * use case: it has no provision for updating an existing document inside 
+         * a collection.
+         */
         private static void UpdateWithNewTest(IMongoCollection<BsonDocument> collection, string deviceId, TestPayLoad test)
        {
            var day = "ISODate(\"" + DateTime.UtcNow.Date.ToString("yyyy-MM-dd") + "\")";
 
-            var filter = "{'deviceid': '" + deviceId + "', nsamples: {$lt: 50}, day: " + day + "}";
+            var filter = "{'deviceid': '" + deviceId + "', nsamples: {$lt: 3}, day: " + day + "}";
 
           var update = Builders<BsonDocument>.Update
                .Inc("nsamples", 1)
@@ -42,6 +73,11 @@ namespace TestMongo
 
             Console.WriteLine(value: "Update Renderer : " + update.Render(collection.DocumentSerializer, collection.Settings.SerializerRegistry));
 
+            /* IMongoCollection(TDocument) UpdateOne method
+             * (IClientSessionHandle, FilterDefinition(TDocument), 
+             * UpdateDefinition(TDocument),
+             * UpdateOptions, CancellationToken)
+             */ 
             collection.UpdateOne(filter, update, new UpdateOptions() { IsUpsert = true });
 
 
