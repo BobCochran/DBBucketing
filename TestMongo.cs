c115911b92523cf7498ba32f42888d87d220d5f4
BobCochran <r2cochran2@gmail.com>
2019-02-02 11:59:47 -0500

    Still producing an invalid BsonDocument

--- TestMongo.cs
+++ TestMongo.cs
@@ -66,8 +66,7 @@ namespace TestMongo
  
            var myday = new BsonDocument { { "day", DateTime.UtcNow.Date } };
 
-
-            var data1 = new BsonDocument { { "deviceid", deviceId, "nsamples", 0, myday } };
+            var data1 = new BsonDocument { { "deviceid", deviceId }, { "nsamples", 0 }, { "day", DateTime.UtcNow.Date } };
 
             var builder = Builders<BsonDocument>.Filter;
 
